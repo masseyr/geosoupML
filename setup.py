115d3854b6354c92a0b45dd90fe60337466f121a
Richard Massey <rm885@nau.edu>
2020-04-26 19:01:53 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="geosoup",
+    name="geosoupML",
     version="0.1.0",
     author="Richard Massey",
     author_email="rm885@nau.edu",
