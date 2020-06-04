207395d2898cacde61b0e2c5f26afcf7095a3bcb
Richard <masseyr44@gmail.com>
2020-06-04 12:21:58 -0700

    added Mahalanobis to __init__

--- geosoupML/__init__.py
+++ geosoupML/__init__.py
@@ -1,3 +1,3 @@
-from geosoupML.distance import Distance, Euclidean
+from geosoupML.distance import Distance, Euclidean, Mahalanobis
 from geosoupML.regression import MRegressor, RFRegressor, HRFRegressor
 from geosoupML.samples import Samples
